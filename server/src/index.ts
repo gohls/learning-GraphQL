60a003c658708bc1bc1da5d098cce810d7b0f6e6
gohls <sagohl@gmail.com>
2023-07-12 12:08:33 -0700

    add mock data

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,17 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./schema";
+import { addMocksToSchema } from "@graphql-tools/mock";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { mocks } from "./mocks";
 
 async function startApolloServer() {
-  const server = new ApolloServer({typeDefs});
+  const server = new ApolloServer({
+    schema: addMocksToSchema({
+      schema: makeExecutableSchema({ typeDefs }),
+      mocks
+    }),
+  });
   const { url } = await startStandaloneServer(server);
   console.log(`
     🚀  Server is running!
