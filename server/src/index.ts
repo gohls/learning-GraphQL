409cfcddaa342eb54707e95961ec61513d6f0cb5
gohls <sagohl@gmail.com>
2023-07-13 09:13:06 -0700

    replace mock data with resolvers

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,24 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./schema";
-import { addMocksToSchema } from "@graphql-tools/mock";
-import { makeExecutableSchema } from "@graphql-tools/schema";
-import { mocks } from "./mocks";
+import { TrackAPI } from "./datasources/track-api";
+import { resolvers } from "./resolvers";
 
 async function startApolloServer() {
   const server = new ApolloServer({
-    schema: addMocksToSchema({
-      schema: makeExecutableSchema({ typeDefs }),
-      mocks
-    }),
+    typeDefs,
+    resolvers,
   });
-  const { url } = await startStandaloneServer(server);
+  const { url } = await startStandaloneServer(server, {
+    context: async () => {
+      const { cache } = server;
+      return {
+        dataSources: {
+          trackAPI: new TrackAPI({ cache }),
+        },
+      };
+    },
+  });
   console.log(`
     🚀  Server is running!
     📭  Query at ${url}
