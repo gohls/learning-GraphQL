8e90fca3143b005d540484b9c1eb4b9c46496be0
gohls <sagohl@gmail.com>
2023-07-13 13:36:49 -0700

    add Mutation to increment views

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -6,6 +6,21 @@ export const typeDefs = gql`
     track(id: ID!): Track
   }
 
+  type Mutation {
+    incrementTrackViews(id: ID!): IncrementTrackViewsResp!
+  }
+
+  tyle IncrementTrackViewsResp {
+    "Similar to HTTP status code, represents the status of the mutation"
+    code: Int!
+    "Indicates whether the mutation was successful"
+    success: Boolean!
+    "Human-readable message for the UI"
+    message: String!
+    "Newly updated track after mutation"
+    track: Track
+  }
+
   """
   A track is a group of Modules that teaches about a specific topic
   """
